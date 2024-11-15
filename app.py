28b3dab346f159565c40fa168c4af28b86689a58
Adapala Mahesh <139545335+Star-techies2@users.noreply.github.com>
2024-11-15 18:16:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ DOWNLOAD_FOLDER = './downloads'
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 # Path to the ChromeDriver executable
-CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver-linux64'  # Update this path
+CHROMEDRIVER_PATH = '/app/chromedriver-linux64'  # Update this path
 
 # Function to extract PDF links using Selenium in headless mode
 def extract_pdf_links():
