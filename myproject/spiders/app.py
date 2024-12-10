4ccd32455d250b99cb18cbbfc2f38f1d4857acf4
Adapala Mahesh <139545335+Star-techies2@users.noreply.github.com>
2024-12-10 23:50:08 +0530

    Update app.py

--- myproject/spiders/app.py
+++ myproject/spiders/app.py
@@ -11,13 +11,13 @@ def index():
 @app.route('/run_spider', methods=['POST'])
 def run_spider():
     # Run the Scrapy spider
-    subprocess.run(['python', 'test.py'])
+    subprocess.run(['python', 'test.py'], cwd='/app/myproject/spiders')
     return redirect(url_for('index'))
 
 @app.route('/show_results', methods=['POST'])
 def show_results():
     # Read the contents of the text file
-    file_path = 'bcbsla_webscraping_links.txt'
+    file_path = '/app/myproject/spiders/bcbsla_webscraping_links.txt'
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
@@ -26,4 +26,4 @@ def show_results():
     return render_template('results.html', content=content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
